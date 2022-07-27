e9fea74c8f85edee52622113d14d344aeedd11cf
arushwolf <h.jaberul2020@gmail.com>
2022-07-27 00:10:18 -0400

    So I have figured out how to make it work...BUT it keeps printing the first integer, rather than every single one...I am guessing it has something to do with the "wordRemover" function?? Oh, and I added another function to...ease the workload in main I guess...

--- simpleCuriousity001.cpp
+++ simpleCuriousity001.cpp
@@ -3,23 +3,35 @@
 
 using namespace std;
 
-//void wordRemover(string mainString, string subString);
-void numberIsolater(string s);
+void wordRemover(string mainString, string subString);
+string numberIsolater(string s);
+int wordCounter(string s);
 
 int main()
 {
 
 	string userInput;
 	getline(cin, userInput);
-	cout << userInput << endl;
-	numberIsolater(userInput);
+	int words = wordCounter(userInput);
+	string* array = new string[words]; //this is a dynamic array created to store the integers...
+	for (int i = 0; i < words; i++)
+	{
+		array[i] = "NULL";
+		array[i] = numberIsolater(userInput);
+	}
+
+	for (int i = 0; i < words; i++)
+	{
+		if (array[i] != "NULL")
+			cout << array[i] << endl;
+	}
 
 }
 
 /*Ok, so I will first define the "numberIsolater" function,
 as it is much more complicated than the "wordRemover" function*/
 
-void numberIsolater(string s)
+string numberIsolater(string s)
 {
 	stringstream stringS;
 
@@ -34,8 +46,31 @@ void numberIsolater(string s)
 		stringS >> word; //as stated before, taking each word and storing it
 
 		if (stringstream(word) >> x)
-			cout << x << " ";
-
-		word = "";
+		{
+			wordRemover(s, word);
+			return word;
+		}
 	}
+}
+
+void wordRemover(string mainString, string subString)
+{
+	size_t pos = mainString.find(subString); //search for substring
+
+	if (pos != string::npos)
+		mainString.erase(pos, subString.length());//erase the substring once found
+}
+
+int wordCounter(string s)
+{
+	int words = 0;
+
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (isspace(s[i]))
+			words++; //count the number of spaces
+	}
+
+	words++; //add one more to have an accurate count (assuming there is no extra spaces
+	return words;
 }
