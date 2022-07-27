f63afff927438187894d8823a07b81f73bf5b2ec
arushwolf <h.jaberul2020@gmail.com>
2022-07-26 23:11:25 -0400

    In this initial version, I will be using the "numberIsolater" as a void function to see if it does return anything. If successful, I will tweak it and define the "wordRemover" function to accomplish its intended goal.

--- simpleCuriousity001.cpp
+++ simpleCuriousity001.cpp
@@ -3,15 +3,36 @@
 
 using namespace std;
 
-void wordRemover(string mainString, string subString);
+//void wordRemover(string mainString, string subString);
 void numberIsolater(string s);
 
 int main()
 {
 
-
+	string userInput;
+	cin >> userInput;
+	numberIsolater(userInput);
 
 }
 
 /*Ok, so I will first define the "numberIsolater" function,
 as it is much more complicated than the "wordRemover" function*/
+
+void numberIsolater(string s)
+{
+	stringstream stringS;
+
+	stringS << s; //this should take the string and put it into the stream
+
+	string word; //to store each individual word of the string
+	int x; //to store an integer once found
+
+	//this loop will run until the end of the string
+	while (!stringS.eof())
+	{
+		stringS >> word; //as stated before, taking each word and storing it
+
+		if (stringstream(word) >> x)
+			cout << x << " ";
+	}
+}
