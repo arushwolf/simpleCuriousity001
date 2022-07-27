0d8fa9ac65a7347ddaec47076e06df013df45d0e
arushwolf <h.jaberul2020@gmail.com>
2022-07-26 23:02:57 -0400

    So this is the basic outline of the functions that I will be using to accomplish my goal of extracting integers from any string input.

---
+++ simpleCuriousity001.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+void wordRemover(string mainString, string subString);
+void numberIsolater(string s);
+
+int main()
+{
+
+
+
+}
+
+/*Ok, so I will first define the "numberIsolater" function,
+as it is much more complicated than the "wordRemover" function*/
